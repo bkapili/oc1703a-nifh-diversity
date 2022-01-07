8ae6ee77d1cfd2780350db73269072f3017dbdb7
bkapili <benn.kapili@gmail.com>
2022-01-07 11:14:33 -0800

    Add fastqc script

---
+++ scripts/fastqc.sh
@@ -0,0 +1,27 @@
+# -------------------------------------------------------------
+# Script purpose: Summarize forward and reverse read quality scores 
+#                 for each sample
+#
+# Input:  Raw paired-end fastq files
+#
+# Output: HTML files containing summaries of forward and reverse
+#         read quality scores per sample
+#
+# Arguments:
+#   * Name of sequencing run ("dekas12"" or "dekas13")
+#
+# To run on dekas12: bash cutadapt.sh dekas12
+# To run on dekas13: bash cutadapt.sh dekas13
+# -------------------------------------------------------------
+
+# Read argument
+SEQ_RUN=$1
+
+# Change directory
+cd ../data/$SEQ_RUN/raw
+
+# Run fastqc
+fastqc *.fastq.gz
+
+# Move to new directory
+mkdir ./fastqc && mv *_fastqc* ./fastqc
